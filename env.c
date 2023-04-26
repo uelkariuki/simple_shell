41464d07666a0a7102820927bb637d762d1064b6
uelkariuki <cratzuel@gmail.com>
2023-04-26 23:47:38 +0000

    hsh shell

---
+++ env.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * env_func -function to display environment
+ */
+
+void env_func(void)
+{
+	char **env = environ;
+
+	while (*env)
+	{
+		/*write(STDOUT_FILENO, *env, length);*/
+		/*write(STDOUT_FILENO, "\n", 2);*/
+		printf("%s\n", *env);
+		env++;
+
+	}
+
+}
