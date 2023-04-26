b805ef0c64de1c21da004f4bce989f4734eff6a2
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-26 21:18:58 +0300

    check simple_shell.c

--- shell_exer/main.h
+++ shell_exer/main.h
@@ -1,16 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
-#ifndef MAIN_H
-#define MAIN_H
+#include <sys/types.h>
 
 #define BUFSIZE 100
 /*function parameters*/
-void path_file();
+pid_t fork_child(char *buffer);
+char error_eof(ssize_t prompt, char *buffer);
 void file_path(char *command);
 char *strtok(char *str, const char *delim);
 int _setenv(const char *name, const char *value, int overwrite);
--- shell_exer/simple_shell.c
+++ shell_exer/simple_shell.c
@@ -1,81 +1,72 @@
 #include "main.h"
-
 /**
  * main - prints out the results of the user input shell cmd
  * @argc: no of arguments in a argv
  * @argv: array pointer for argc
- * return: 0
+ * Return - 0 on success and non-sero on failure
  */
-
 int main(int argc, char **argv)
 {
-	pid_t pid;
 	char *buffer, *av[100];
-	size_t buffer_size = BUFSIZE;
+	size_t bufsize = 100;
 	int status, executed_execve = 0;
 	ssize_t prompt;
-	(void)argc;
-	(void)argv;
-	
-	buffer = malloc(sizeof(char) * buffer_size);
+	pid_t pid;
+	(void)argc, (void)argv;
+
+	buffer = malloc(sizeof(char) * bufsize);
+
 	if (buffer == NULL)
 	{
 		perror("Error: Allocating malloc");
 		exit(EXIT_FAILURE);
 	}
-	else
+	while (1)
 	{
-		while (1)
+		printf("$ ");
+		prompt = getline(&buffer, &bufsize, stdin);
+		if (prompt == EOF)
 		{
-			printf("$ ");
-			/*without getline function*/
-			prompt = getline(&buffer, &buffer_size, stdin);
-			if (prompt == EOF)
+			perror("Error: end of file");
+			free(buffer);
+			exit(EXIT_FAILURE);
+		}
+		buffer[strcspn(buffer, "\n")] = '\0';
+		exit_status(buffer);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error: while creating fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			av[0] = buffer;
+			av[1] = NULL;
+			if( execve(av[0], av, NULL) == 1)
 			{
-				perror("Error: reached end of file\n");
+				perror("error: executing execve");
 				free(buffer);
 				exit(EXIT_FAILURE);
 			}
-			
-			buffer[strcspn(buffer, "\n")] = '\0';
-			/*exit_status function*/
-			exit_status(buffer);
-
-			pid = fork();
-			if (pid == -1)
+		}
+		else
+		{
+			wait(&status);
+			if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
 			{
-				perror("Error: while creating fork");
-				exit(EXIT_FAILURE);
+				executed_execve = 1;
 			}
-			else if (pid == 0)
-			{
-				av[0] = buffer;
-				av[1] = NULL;
-				if( execve(av[0], av, NULL) == 1)
-				{
-					perror("error: executing execve");
-					free(buffer);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				wait(&status);
-				if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
-				{
-					executed_execve = 1;
-				}
-			}
-			if (!executed_execve)
-			{
-				system(buffer);
-			}
-			else
-			{
-				executed_execve = 0;
-			}
+		}
+		if (!executed_execve)
+		{
+			system(buffer);
+		}
+		else
+		{
+			executed_execve = 0;
 		}
 	}
 	free(buffer);
 	return (0);
-}	
+}
