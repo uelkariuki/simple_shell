7ed6228e1989f2ca2831a099a8843a158be98aeb
francis waitiruka <franciswaitiruka777@gmail.com>
2023-04-27 09:25:17 +0300

    retrial of unfinished

--- shellfl/simple_shell1.c
+++ shellfl/simple_shell1.c
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
 {
 	char *buffer, *av[100];
 	size_t bufsize = 100;
+	int status, executed_execve = 0;
 	ssize_t prompt;
-	int status;
 	pid_t pid;
 	(void)argc, (void)argv;
 
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 
 		buffer[strcspn(buffer, "\n")] = '\0';
 
-		/*exit_status(buffer);*/
+		exit_status(buffer);
 
 		pid = fork();
 		if (pid == -1)
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
 		{
 			av[0] = buffer;
 			av[1] = NULL;
-			if( execve(av[0], av, NULL) == -1)
+			if( execve(av[0], av, NULL) == 1)
 		
 			{
 				perror("error: executing execve");
@@ -58,6 +58,18 @@ int main(int argc, char **argv)
 		else
 		{
 			wait(&status);
+			if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+			{
+				executed_execve = 1;
+			}
+		}
+		if (!executed_execve)
+		{
+			system(buffer);
+		}
+		else
+		{
+			executed_execve = 0;
 		}
 	}
 	free(buffer);
